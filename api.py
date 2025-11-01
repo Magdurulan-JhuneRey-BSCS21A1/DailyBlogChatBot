9c00226951b38d17ff5580863542f4c3e8476eb4
Jhune Rey E. Magdurulan <mjhunerey28@gmail.com>
2025-11-01 18:00:16 +0800

    Allow Multiple Req

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import json
+import os
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
@@ -47,4 +48,5 @@ def chat():
 # Run server
 # -----------------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
