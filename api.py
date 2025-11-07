aaa1a335518a3336dfdbcc1d1a3dc9205df9e9ce
Jhune Rey E. Magdurulan <154797806+Magdurulan-JhuneRey-BSCS21A1@users.noreply.github.com>
2025-11-07 19:02:46 +0800

    Add download for punkt_tab NLTK data

--- api.py
+++ api.py
@@ -13,6 +13,7 @@ import numpy as np
 
 # Download NLTK data
 nltk.download("punkt", quiet=True)
+nltk.download("punkt_tab", quiet=True)   # <-- add this line
 nltk.download("wordnet", quiet=True)
 nltk.download("omw-1.4", quiet=True)
 nltk.download("stopwords", quiet=True)
